f9b4e136c9aa9481e191c37dab21d7ef92e520e3
Sergey Kojin <sergey.kojin@gmail.com>
2017-03-12 20:11:22 +0300

    add tests for hash multi values

--- test/complex_hash_conditions_test.rb
+++ test/complex_hash_conditions_test.rb
@@ -3,6 +3,7 @@ require File.dirname(__FILE__) + '/test_helper'
 
 class ComplexHashConditionsTest < ActiveSupport::TestCase
   load_schema
+  
   class Folder < ActiveRecord::Base
     has_many :messages
   end
@@ -13,6 +14,9 @@ class ComplexHashConditionsTest < ActiveSupport::TestCase
 
   class MessageClickedAt < Message
     counter_cache_with_conditions :folder, :clicked_messages_count, :clicked_at => true
+  end
+  class MessageMulti < Message
+    counter_cache_with_conditions :folder, :unread_messages_count, :unread => true, :status => 'active'
   end
 
   def test_created_at_on_to_off
@@ -30,6 +34,22 @@ class ComplexHashConditionsTest < ActiveSupport::TestCase
   end
 
   
+  def test_multi_on_to_off
+    f, m = build_fixture(MessageMulti, :unread => true, :status => 'active')
+    assert_equal 1, f.reload.unread_messages_count, "should increment on create"
+    m.update_attribute :status, 'archived'
+    assert_equal 0, f.reload.unread_messages_count, "should decrement on update when condition not match"
+  end
+
+  def test_multi_off_to_on
+    f, m = build_fixture(MessageMulti)
+    assert_equal 0, f.reload.unread_messages_count, "no increment since off"
+    m.update_attribute :unread, true
+    assert_equal 0, f.reload.unread_messages_count, "no increment since off"
+    m.update_attribute :status, 'active'
+    assert_equal 1, f.reload.unread_messages_count, "should increment on update when all conditions match"
+  end
+
   def teardown
     Message.delete_all
     Folder.delete_all
@@ -38,7 +58,7 @@ class ComplexHashConditionsTest < ActiveSupport::TestCase
   private
   def build_fixture(message_class, attributes = {})
     f = Folder.create
-    m = MessageClickedAt.new(folder: f)
+    m = message_class.new(folder: f)
     m.attributes = attributes
     m.save!
     [f, m]
--- test/schema.rb
+++ test/schema.rb
@@ -12,6 +12,7 @@ ActiveRecord::Schema.define do
   create_table "messages", :force => true do |t|
     t.belongs_to :folder
     t.boolean    :unread, :null => false, :default => true
+    t.string     :status
     t.datetime   :clicked_at
   end
 
